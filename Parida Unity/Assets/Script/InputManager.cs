fec6dc1a662cb6ab7ff2b4681c36575a44efa684
Ranju Raveendran <ranjurave@gmail.com>
2020-11-02 14:44:08 +1300

    Initial Commit
    Buttons working. No background touch detected.
    Objects can be spawned. (4 furniture)
    Until video 3

---
+++ Parida Unity/Assets/Script/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    public Camera arCam;
+    public ARRaycastManager raycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private Touch touch;
+
+    // Update is called once per frame
+    void Update()
+    {
+        touch = Input.GetTouch(0);
+
+        if (Input.touchCount < 0 || touch.phase != TouchPhase.Began) return;
+
+        if (IsPointerOverUI(touch)) return;
+
+        if (Input.GetMouseButton(0)) {
+            Ray ray = arCam.ScreenPointToRay(touch.position);
+            if(raycastManager.Raycast(ray, hits)){
+                Pose pose = hits[0].pose;
+                Instantiate(DataHandler.Instance.furniture, pose.position, pose.rotation);
+            }
+        }
+    }
+
+    bool IsPointerOverUI(Touch touch) {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
+    }
+}
