78fa1aff04d001cb756ef8dde1eabf7143958a4a
Ranju Raveendran <ranjurave@gmail.com>
2020-11-04 12:15:48 +1300

    Plane detection works
    Spawn only when plane detected. Controlled with a boolean.
    Works in this commit.

--- Parida Unity/Assets/Script/ButtonManager.cs
+++ Parida Unity/Assets/Script/ButtonManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour {
     private Button btn;
     public GameObject Furniture;
+
     // Start is called before the first frame update
     void Start() {
         btn = GetComponent<Button>();
--- Parida Unity/Assets/Script/InputManager.cs
+++ Parida Unity/Assets/Script/InputManager.cs
@@ -12,10 +12,13 @@ public class InputManager : MonoBehaviour {
     private Touch touch;
     public GameObject crosshair;
     private Pose pose;
+    private bool CanPlace;
 
     void Start() {
-        crosshair.SetActive(true);
+        crosshair.SetActive(false);
+        CanPlace = false;
     }
+    
     // Update is called once per frame
     void Update() {
         CrosshairCalculation();
@@ -24,8 +27,10 @@ public class InputManager : MonoBehaviour {
         if (Input.touchCount < 0 || touch.phase != TouchPhase.Began) return;
 
         if (IsPointerOverUI(touch)) return;
-        
-        if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began) {
+
+        //if (IsPointerOverObject()) {  }
+
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began && CanPlace) {
         Instantiate(DataHandler.Instance.furniture, crosshair.transform.position, crosshair.transform.rotation);
         }
 
@@ -44,22 +49,18 @@ public class InputManager : MonoBehaviour {
         Ray ray = arCam.ScreenPointToRay(origin);
 
         if (raycastManager.Raycast(ray, hits)) {
+            crosshair.SetActive(true);
+            CanPlace = true;
             pose = hits[0].pose;
             crosshair.transform.position = pose.position;
             crosshair.transform.eulerAngles = new Vector3(0, 0, 0);
         }
     }
 
-    // Need to try this...................................
-    //void CrosshairCalculation2() {
-    //    List<ARRaycastHit> hits2 = new List<ARRaycastHit>();
-    //    raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits2, TrackableType.Planes);
-
-    //    if (hits2.Count > 0) {
-    //        //crosshair.SetActive(true);
-    //        crosshair.transform.position = hits2[0].pose.position;
-    //        crosshair.transform.rotation = hits2[0].pose.rotation;
-
-    //    }
+    //bool IsPointerOverObject() {
+    //    Vector3 origin = arCam.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
+    //    Ray ray = arCam.ScreenPointToRay(origin);
+    //    //raycast
+    //    return true;
     //}
 }
