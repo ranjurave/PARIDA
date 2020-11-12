c4b37e04500ea5c6a24386ef79f579779bb56f7f
Ranju Raveendran <ranjurave@gmail.com>
2020-11-12 13:11:26 +1300

    Works fine.
    Crosshair displays ony when objects are placable.
    Cannot place on already spawned objects.
    A screen log also created and works fine.

--- Parida Unity/Assets/Script/InputManager.cs
+++ Parida Unity/Assets/Script/InputManager.cs
@@ -11,19 +11,19 @@ public class InputManager : MonoBehaviour {
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
     private Touch touch;
     public GameObject crosshair;
-    private Pose pose;
     private bool CanPlace;
+    private Pose pose;
 
     void Start() {
-        crosshair.SetActive(false);
         CanPlace = false;
     }
 
     // Update is called once per frame
     void Update() {
+        crosshair.SetActive(CanPlace);
         CrosshairCalculation();
+
         touch = Input.GetTouch(0);
-        IsPointerOverObject();
 
         if (Input.touchCount < 0 || touch.phase != TouchPhase.Began) return;
 
@@ -52,15 +52,15 @@ public class InputManager : MonoBehaviour {
         Ray ray = arCam.ScreenPointToRay(origin);
 
         if (raycastManager.Raycast(ray, hits)) {
-            crosshair.SetActive(true);
-            CanPlace = true;
+            CanPlace = IsPointerOverObject();
+            crosshair.SetActive(CanPlace);
             pose = hits[0].pose;
             crosshair.transform.position = pose.position;
             crosshair.transform.eulerAngles = new Vector3(0, 0, 0);
         }
     }
 
-    
+
     void DoubleTouchObject(Touch touch) {
         Pose rPose;
         Ray touchRay = Camera.main.ScreenPointToRay(touch.position);
@@ -73,18 +73,24 @@ public class InputManager : MonoBehaviour {
         }
     }
 
-    void IsPointerOverObject() {
+    bool IsPointerOverObject() {
         Vector3 origin = arCam.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
         Ray pointingRay = arCam.ScreenPointToRay(origin);
         RaycastHit objectHit;
 
         if (Physics.Raycast(pointingRay, out objectHit)) {
             if (objectHit.collider) {
-                crosshair.SetActive(false);
-            } else {
-                crosshair.SetActive(true);
+                return false;
             }
+            return true;
         }
+        return true;
     }
 
+    private void OnGUI() {
+        GUIStyle myRectStyle = new GUIStyle(GUI.skin.textField);
+        myRectStyle.fontSize = 25;
+        myRectStyle.normal.textColor = Color.red;
+        GUI.Box(new Rect(new Vector2(100, 100), new Vector2(200, 200)), CanPlace.ToString(), myRectStyle);
+    }
 }
