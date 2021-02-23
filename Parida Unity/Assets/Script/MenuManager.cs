693a06960150660ce4787561e94c935c4caed307
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2021-02-23 15:46:03 +1300

    More Menu added
    not working atm

--- Parida Unity/Assets/Script/MenuManager.cs
+++ Parida Unity/Assets/Script/MenuManager.cs
@@ -7,51 +7,92 @@ using System;
 
 public class MenuManager : MonoBehaviour {
     public GameObject selectedFocus;
-    private GameObject previousPanel;
-    private GameObject curentPanel;
-    private GameObject tempPanel;
     public GameObject onScreenUI;
     public GameObject roomSelection;
     public GameObject styleSelection;
     public GameObject focusObjectSelection;
     public GameObject focusObjectTypeSelection;
+    public GameObject furnitureToPlace;
 
     void Start() {
         roomSelection.SetActive(true);
         styleSelection.SetActive(false);
         focusObjectTypeSelection.SetActive(false);
         focusObjectSelection.SetActive(false);
+        onScreenUI.SetActive(false);
+        furnitureToPlace.SetActive(false);
     }
 
     public void StyleSelection() {
-        previousPanel = roomSelection;
-        curentPanel = styleSelection;
-        previousPanel.SetActive(false);
-        curentPanel.SetActive(true);
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(true);
+        furnitureToPlace.SetActive(false);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(false);
     }
     public void FocusObjectTypeSelection() {
-        previousPanel = curentPanel;
-        curentPanel = focusObjectTypeSelection;
-        previousPanel.SetActive(false);
-        curentPanel.SetActive(true);
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(true);
+        focusObjectSelection.SetActive(false);
+        furnitureToPlace.SetActive(false);
     }
     public void FocusObjectSelection() {
-        previousPanel = curentPanel;
-        curentPanel = focusObjectSelection;
-        previousPanel.SetActive(false);
-        curentPanel.SetActive(true);
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(true);
+        furnitureToPlace.SetActive(false);
     }
     public void ObjectSelected() {
-        previousPanel = curentPanel;
-        curentPanel = onScreenUI;
-        previousPanel.SetActive(false);
-        curentPanel.SetActive(true);
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(false);
+        furnitureToPlace.SetActive(false);
+        onScreenUI.SetActive(true);
     }
-    public void BackButton() {
-        tempPanel = curentPanel;
-        curentPanel = previousPanel;
-        previousPanel = tempPanel;
-        previousPanel.SetActive(false);
-        curentPanel.SetActive(true);
+    public void BackButtonFocusObject() {
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(true);
+        focusObjectSelection.SetActive(false);
+    }
+    public void BackButtonFocusObjectType() {
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(true);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(false);
+    }
+    public void BackButtonStyleSelection() {
+        roomSelection.SetActive(true);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(false);
+    }
+    public void BackOnScreenUI() {
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(true);
+        onScreenUI.SetActive(false);
+    }
+
+    // TODO To check if focus object is placed
+    public void AddMoreObjects() {  
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(false);
+        onScreenUI.SetActive(false);
+        furnitureToPlace.SetActive(true);
+    }
+    public void BackAddMoreObjects() {
+        roomSelection.SetActive(false);
+        styleSelection.SetActive(false);
+        focusObjectTypeSelection.SetActive(false);
+        focusObjectSelection.SetActive(false);
+        onScreenUI.SetActive(true);
+        furnitureToPlace.SetActive(false);
     }
 }
